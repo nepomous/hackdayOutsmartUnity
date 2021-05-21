ee6d4eeaf779403f90d7de9cce6adb74180ccfff
Guilherme Tadashi Adaniya <54946645+guilherme-adaniya@users.noreply.github.com>
2021-05-21 16:44:48 -0300

    Feat/OpossumEnemy

--- Hackday Outsmart/Assets/Scripts/EnemyHP.cs
+++ Hackday Outsmart/Assets/Scripts/EnemyHP.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class EnemyHP : MonoBehaviour
 {
+    public int enemyHP;
+    private int currentHP;
+    private Animator animator;
+    private bool isDead;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHP = enemyHP;
+        animator = transform.parent.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(currentHP <= 0) {
+            isDead = true;
+            animator.SetBool("Dead", isDead);
+            StartCoroutine("KillSwitch");
+        }
+    }
+
+    public void TakeDamage(int damage) {
+        currentHP -= damage;
+    }
+
+    IEnumerator KillSwitch() {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(transform.parent.gameObject);
     }
 }
